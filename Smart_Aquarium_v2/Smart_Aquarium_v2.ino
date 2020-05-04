8f7ae615619f85ef88083866207fbbf4e27b58ad
HabonRoof <johnson35762@gmail.com>
2020-05-04 20:15:01 +0800

    新增Arduino Code

---
+++ Smart_Aquarium_v2/Smart_Aquarium_v2.ino
@@ -0,0 +1,27 @@
+/*
+ * Smart Aquarium V2.0
+ * 2020/05/03
+ * https://www.instagram.com/habonroof/
+ */
+
+#include <LiquidCrystal_I2C.h>
+#include <LWiFi.h>
+#include <WiFiClient.h>
+#include "MCS.h"
+#include <LRTC.h>
+#include <WiFiUDP.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+#define _SSID "johnson"                                                            // Assign AP ssid / password here
+#define _KEY  "j8701292929"
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
